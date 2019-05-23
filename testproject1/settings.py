517e7c75ffefeaacb8d341076e558f3c64b18e0f
So-shi <king.eskey@gmail.com>
2019-05-23 14:08:42 +0900

    thrd commit: modified setting.py

--- testproject1/settings.py
+++ testproject1/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -26,7 +26,7 @@ SECRET_KEY = 'c%mq5sy=ows^y8+ay+5t!g_mjhcojvl(h+nzr-f9%sw5ujwsz+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
